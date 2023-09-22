7afe7293ab39d247216d9543a6d0fd3bda8c8d80
PhilippThoelke <philipp.thoelke@posteo.de>
2023-09-22 16:03:53 -0400

    Format connectivity

--- src/goofi/nodes/eeg/connectivity.py
+++ src/goofi/nodes/eeg/connectivity.py
@@ -1,5 +1,6 @@
 import numpy as np
 from biotuner.harmonic_connectivity import harmonic_connectivity
+
 from goofi.data import Data, DataType
 from goofi.node import Node
 from goofi.params import FloatParam, IntParam
@@ -15,13 +16,16 @@ class Connectivity(Node):
         }
 
     def config_params():
-        return {"biotuner": {"method": 'harmsim',
-                             "n_peaks": IntParam(5, 1, 10),
-                             "f_min": FloatParam(2.0, 0.1, 50.0),
-                             "f_max": FloatParam(30.0, 1.0, 100.0),
-                             "precision": FloatParam(0.1, 0.01, 10.0),
-                             "peaks_function": "EMD"}}
-                             
+        return {
+            "biotuner": {
+                "method": "harmsim",
+                "n_peaks": IntParam(5, 1, 10),
+                "f_min": FloatParam(2.0, 0.1, 50.0),
+                "f_max": FloatParam(30.0, 1.0, 100.0),
+                "precision": FloatParam(0.1, 0.01, 10.0),
+                "peaks_function": "EMD",
+            }
+        }
 
     def process(self, data: Data):
         if data is None:
@@ -30,20 +34,24 @@ class Connectivity(Node):
         data.data = np.squeeze(data.data)
         if data.data.ndim != 2:
             raise ValueError("Data must be 2D")
-        
-        harm_conn = compute_conn_matrix_single(data.data, data.meta['sfreq'], peaks_function=self.params["biotuner"]["peaks_function"].value,
-                                               min_freq=self.params["biotuner"]["f_min"].value,
-                                               max_freq=self.params["biotuner"]["f_max"].value,
-                                               precision=self.params["biotuner"]["precision"].value,
-                                               n_peaks=self.params["biotuner"]["n_peaks"].value,
-                                               metric=self.params["biotuner"]["method"].value)
+
+        harm_conn = compute_conn_matrix_single(
+            data.data,
+            data.meta["sfreq"],
+            peaks_function=self.params["biotuner"]["peaks_function"].value,
+            min_freq=self.params["biotuner"]["f_min"].value,
+            max_freq=self.params["biotuner"]["f_max"].value,
+            precision=self.params["biotuner"]["precision"].value,
+            n_peaks=self.params["biotuner"]["n_peaks"].value,
+            metric=self.params["biotuner"]["method"].value,
+        )
 
         return {"matrix": (harm_conn, data.meta)}
 
 
-
-
-def compute_conn_matrix_single(data, sf, peaks_function="EMD", min_freq=2, max_freq=45, precision=0.1, n_peaks=5, metric='harmsim'):
+def compute_conn_matrix_single(
+    data, sf, peaks_function="EMD", min_freq=2, max_freq=45, precision=0.1, n_peaks=5, metric="harmsim"
+):
     bt_conn = harmonic_connectivity(
         sf=sf,
         data=data,
@@ -54,5 +62,4 @@ def compute_conn_matrix_single(data, sf, peaks_function="EMD", min_freq=2, max_f
         n_peaks=n_peaks,
     )
     bt_conn.compute_harm_connectivity(metric=metric, save=False, graph=False)
-    print(bt_conn.conn_matrix)
     return bt_conn.conn_matrix
