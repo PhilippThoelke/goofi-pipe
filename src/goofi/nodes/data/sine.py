1d8270a9c6d6b356e110bc772dc6b5ed87c3cb4d
PhilippThoelke <philipp.thoelke@posteo.de>
2023-09-21 21:23:37 -0400

    Implement higher sampling frequency

--- src/goofi/nodes/data/sine.py
+++ src/goofi/nodes/data/sine.py
@@ -10,12 +10,23 @@ from goofi.params import FloatParam
 class Sine(Node):
     def config_params():
         return {
-            "sine": {"frequency": FloatParam(1.0, 0.1, 30.0)},
+            "sine": {"frequency": FloatParam(1.0, 0.1, 30.0), "sampling_frequency": FloatParam(1000.0, 1.0, 1000.0)},
             "common": {"autotrigger": True},
         }
 
     def config_output_slots():
         return {"out": DataType.ARRAY}
 
+    def setup(self):
+        self.last_trigger = time.time()
+
     def process(self):
-        return {"out": (np.array([np.sin(time.time() * np.pi * 2 * self.params.sine.frequency.value)]), {})}
+        meta = {"sfreq": self.params.sine.sampling_frequency.value}
+
+        t = time.time()
+        dt = t - self.last_trigger
+        xs = np.arange(t, t + dt, 1 / self.params.sine.sampling_frequency.value)
+        data = np.sin(xs * np.pi * 2 * self.params.sine.frequency.value)
+
+        self.last_trigger = t
+        return {"out": (data, meta)}
