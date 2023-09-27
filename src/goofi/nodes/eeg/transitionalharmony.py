267216289adcea90a28867b921d7bb446034f57b
PhilippThoelke <philipp.thoelke@posteo.de>
2023-09-26 20:53:08 -0400

    Refactor imports

--- src/goofi/nodes/eeg/transitionalharmony.py
+++ src/goofi/nodes/eeg/transitionalharmony.py
@@ -3,31 +3,34 @@ import numpy as np
 from goofi.data import Data, DataType
 from goofi.node import Node
 from goofi.params import FloatParam, IntParam
-from biotuner.metrics import compute_subharmonics_2lists
-from biotuner.biotuner_object import *
+
 
 class TransitionalHarmony(Node):
     def config_input_slots():
         return {"data": DataType.ARRAY}
 
     def config_output_slots():
-        return {
-            "trans_harm": DataType.ARRAY,
-            "melody": DataType.ARRAY
-        }
+        return {"trans_harm": DataType.ARRAY, "melody": DataType.ARRAY}
 
     def config_params():
         return {
-            "TransitionalHarmony": {
+            "transitional_harmony": {
                 "n_peaks": IntParam(5, 1, 10),
                 "f_min": FloatParam(2.0, 0.1, 50.0),
                 "f_max": FloatParam(30.0, 1.0, 100.0),
                 "precision": FloatParam(0.1, 0.01, 10.0),
                 "peaks_function": "EMD",
                 "delta": IntParam(50, 1, 250),
-                "subharmonics": IntParam(10, 2, 100)
+                "subharmonics": IntParam(10, 2, 100),
             }
         }
+
+    def setup(self):
+        from biotuner.biotuner_object import compute_biotuner
+        from biotuner.metrics import compute_subharmonics_2lists
+
+        self.compute_biotuner = compute_biotuner
+        self.compute_subharmonics_2lists = compute_subharmonics_2lists
 
     def process(self, data: Data):
         if data is None:
@@ -37,33 +40,49 @@ class TransitionalHarmony(Node):
         if data.data.ndim > 1:
             raise ValueError("Data must be 1D")
 
+        # split data in two
         data_len = len(data.data)
-        data1 = data.data[0:int(data_len/2)]
-        data2 = data.data[int(data_len/2):]
-        bt1 = compute_biotuner(sf=data.meta["sfreq"],
-                                peaks_function=self.params["TransitionalHarmony"]["peaks_function"].value,
-                                )
-        bt1.peaks_extraction(data1, n_peaks=self.params["TransitionalHarmony"]["n_peaks"].value,
-                             min_freq=self.params["TransitionalHarmony"]["f_min"].value,
-                                max_freq=self.params["TransitionalHarmony"]["f_max"].value,
-                                precision=self.params["TransitionalHarmony"]["precision"].value)
-        bt2 = compute_biotuner(sf=data.meta["sfreq"],
-                                peaks_function=self.params["TransitionalHarmony"]["peaks_function"].value,
-                                )
-        bt2.peaks_extraction(data2, n_peaks=self.params["TransitionalHarmony"]["n_peaks"].value,
-                             min_freq=self.params["TransitionalHarmony"]["f_min"].value,
-                                max_freq=self.params["TransitionalHarmony"]["f_max"].value,
-                                precision=self.params["TransitionalHarmony"]["precision"].value)
-        result = compute_subharmonics_2lists(bt1.peaks,
-                                            bt2.peaks,
-                                            self.params["TransitionalHarmony"]["subharmonics"].value,
-                                            delta_lim=self.params["TransitionalHarmony"]["delta"].value,
-                                            c=2.1)
-        
+        data1 = data.data[: int(data_len / 2)]
+        data2 = data.data[int(data_len / 2) :]
+
+        # extract peaks from data1
+        bt1 = self.compute_biotuner(
+            sf=data.meta["sfreq"],
+            peaks_function=self.params.transitional_harmony.peaks_function.value,
+        )
+        bt1.peaks_extraction(
+            data1,
+            n_peaks=self.params.transitional_harmony.n_peaks.value,
+            min_freq=self.params.transitional_harmony.f_min.value,
+            max_freq=self.params.transitional_harmony.f_max.value,
+            precision=self.params.transitional_harmony.precision.value,
+        )
+
+        # extract peaks from data2
+        bt2 = self.compute_biotuner(
+            sf=data.meta["sfreq"],
+            peaks_function=self.params.transitional_harmony.peaks_function.value,
+        )
+        bt2.peaks_extraction(
+            data2,
+            n_peaks=self.params.transitional_harmony.n_peaks.value,
+            min_freq=self.params.transitional_harmony.f_min.value,
+            max_freq=self.params.transitional_harmony.f_max.value,
+            precision=self.params.transitional_harmony.precision.value,
+        )
+
+        # compute subharmonics between peaks
+        result = self.compute_subharmonics_2lists(
+            bt1.peaks,
+            bt2.peaks,
+            self.params.transitional_harmony.subharmonics.value,
+            delta_lim=self.params.transitional_harmony.delta.value,
+            c=2.1,
+        )
+
         common_subs, delta_t, sub_tension_final, harm_temp, pairs_melody = result
 
         return {
             "trans_harm": (np.array(sub_tension_final), data.meta),
             "melody": (np.array(pairs_melody), data.meta),
         }
-
--- src/goofi/nodes/eeg/tuningcolors.py
+++ src/goofi/nodes/eeg/tuningcolors.py
@@ -1,84 +1,78 @@
-import numpy as np
-from biotuner.biocolors import audible2visible, scale2freqs, wavelength_to_rgb
-from biotuner.biotuner_object import dyad_similarity
-from biotuner.metrics import tuning_cons_matrix
 import colorsys
+
+import numpy as np
+
 from goofi.data import Data, DataType
 from goofi.node import Node
-from goofi.params import IntParam, FloatParam
+
 
 class TuningColors(Node):
     def config_input_slots():
         return {"data": DataType.ARRAY}
 
     def config_output_slots():
-        return {"hue": DataType.ARRAY,
-                 "saturation": DataType.ARRAY,
-                 "value": DataType.ARRAY}
+        return {"hue": DataType.ARRAY, "saturation": DataType.ARRAY, "value": DataType.ARRAY}
 
-    def config_params():
-        return {}
-                             
+    def setup(self):
+        from biotuner.biocolors import audible2visible, scale2freqs, wavelength_to_rgb
+        from biotuner.biotuner_object import dyad_similarity
+        from biotuner.metrics import tuning_cons_matrix
+
+        self.audible2visible = audible2visible
+        self.scale2freqs = scale2freqs
+        self.wavelength_to_rgb = wavelength_to_rgb
+        self.dyad_similarity = dyad_similarity
+        self.tuning_cons_matrix = tuning_cons_matrix
 
     def process(self, data: Data):
+        """
+        Convert a musical scale into a list of HSV colors based on the scale's frequency values
+        and their averaged consonance.
+        """
         if data is None:
             return None
 
         if data.data.ndim > 1:
             raise ValueError("Data must be 1D")
-        
-        hsvs = viz_scale_colors(data.data, fund=data.data[0])[1:]
 
-        return {"hue": (np.array([x[0] for x in hsvs]), data.meta),
-                "saturation": (np.array([x[1] for x in hsvs]), data.meta),
-                "value": (np.array([x[2] for x in hsvs]), data.meta)}
+        scale = data.data  # a list of frequency ratios representing the musical scale
+        fund = data.data[0]  # fundamental frequency of the scale in Hz
 
+        min_ = 0
+        max_ = 1
+        # convert the scale to frequency values
+        scale_freqs = self.scale2freqs(scale, fund)
+        # compute the averaged consonance of each step
+        scale_cons, _ = self.tuning_cons_matrix(scale, self.dyad_similarity, ratio_type="all")
+        # rescale to match RGB standards (0, 255)
+        scale_cons = (np.array(scale_cons) - min_) * (1 / max_ - min_) * 255
+        scale_cons = scale_cons.astype("uint8").astype(float) / 255
 
+        hsv_all = []
+        for s, cons in zip(scale_freqs, scale_cons):
+            # convert freq in nanometer values
+            _, _, nm, octave = self.audible2visible(s)
+            # convert to RGB values
+            rgb = self.wavelength_to_rgb(nm)
+            # convert to HSV values
+            # TODO: colorsys might be slow
+            hsv = colorsys.rgb_to_hsv(rgb[0] / float(255), rgb[1] / float(255), rgb[2] / float(255))
+            hsv = np.array(hsv)
+            # rescale
+            hsv = (hsv - 0) * (1 / (1 - 0))
+            # define the saturation
+            hsv[1] = cons
+            # define the luminance
+            hsv[2] = 200 / 255
+            hsv = tuple(hsv)
+            hsv_all.append(hsv)
 
-def viz_scale_colors(scale, fund):
-    """
-    Convert a musical scale into a list of HSV colors based on the scale's frequency values
-    and their averaged consonance.
+        # hsv_all is a list of HSV color tuples, one for each scale step, with hue representing the
+        # frequency value, saturation representing the consonance, and luminance set to a fixed value
+        hsvs = hsv_all[1:]
 
-    Parameters:
-        scale (List[float]): A list of frequency ratios representing the musical scale.
-        fund (float): The fundamental frequency of the scale in Hz.
-
-    Returns:
-        hsv_all (List[Tuple[float, float, float]]): A list of HSV color tuples, one for each scale step,
-        with hue representing the frequency value, saturation representing the consonance, and
-        luminance set to a fixed value.
-    """
-
-    min_ = 0
-    max_ = 1
-    # convert the scale to frequency values
-    scale_freqs = scale2freqs(scale, fund)
-    # compute the averaged consonance of each step
-    scale_cons, _ = tuning_cons_matrix(scale, dyad_similarity, ratio_type="all")
-    # rescale to match RGB standards (0, 255)
-    scale_cons = (np.array(scale_cons) - min_) * (1 / max_ - min_) * 255
-    scale_cons = scale_cons.astype("uint8").astype(float) / 255
-
-    hsv_all = []
-    for s, cons in zip(scale_freqs, scale_cons):
-        # convert freq in nanometer values
-        _, _, nm, octave = audible2visible(s)
-        # convert to RGB values
-        rgb = wavelength_to_rgb(nm)
-        # convert to HSV values
-        # TODO: colorsys might be slow
-        hsv = colorsys.rgb_to_hsv(
-            rgb[0] / float(255), rgb[1] / float(255), rgb[2] / float(255)
-        )
-        hsv = np.array(hsv)
-        # rescale
-        hsv = (hsv - 0) * (1 / (1 - 0))
-        # define the saturation
-        hsv[1] = cons
-        # define the luminance
-        hsv[2] = 200 / 255
-        hsv = tuple(hsv)
-        hsv_all.append(hsv)
-
-    return hsv_all
+        return {
+            "hue": (np.array([x[0] for x in hsvs]), data.meta),
+            "saturation": (np.array([x[1] for x in hsvs]), data.meta),
+            "value": (np.array([x[2] for x in hsvs]), data.meta),
+        }
