31e3b98d77dc074c99d8c22bd9fad23d119a5d25
PhilippThoelke <philipp.thoelke@posteo.de>
2023-09-28 21:37:00 -0400

    Implement serial stream node

--- src/goofi/nodes/data/serialstream.py
+++ src/goofi/nodes/data/serialstream.py
@@ -1,77 +1,99 @@
 import time
+from typing import Any, Dict, Tuple
+
 import numpy as np
 import serial
 import serial.tools.list_ports
-from goofi.node import Node
+
 from goofi.data import DataType
-from goofi.params import IntParam, BoolParam
-from typing import Any, Dict, Tuple
+from goofi.node import Node
+from goofi.params import IntParam
 
 
 class SerialStream(Node):
+    ESC = b"\xDB"
+    END = b"\xC0"
+    ESC_END = b"\xDC"
+    ESC_ESC = b"\xDD"
+
     def config_params():
-        return {
-            "serial": {"sfreq": IntParam(1000, 128, 1000), "auto_select": BoolParam(True), "port": "COM4"},
-            "common": {"buffer_seconds": IntParam(5)},
-        }
+        return {"serial": {"sfreq": IntParam(512, 128, 1000), "port": ""}, "common": {"autotrigger": True}}
 
     def config_output_slots():
         return {"out": DataType.ARRAY}
 
     def setup(self):
-        self.last_processed_time = None
-
-    def read_serial(self):
-        serial_buffer = []
-        buffer_times = []
-        if self.params.serial.auto_select.value:
-            ser = serial.Serial(self.detect_serial_port(), 115200, timeout=1)
+        if self.params.serial.port.value:
+            self.ser = serial.Serial(self.params.serial.port.value, 115200, timeout=1)
         else:
-            ser = serial.Serial(self.params.serial.port.value, 115200, timeout=1)
-        print("SERIAL PORT OPENED", ser.name)
-        ESC = b"\xDB"
-        END = b"\xC0"
-        ESC_END = b"\xDC"
-        ESC_ESC = b"\xDD"
+            port = self.detect_serial_port()
+            if port is None:
+                self.ser = None
+                raise RuntimeError("No serial port found.")
+
+            print(f"Found serial port {port}")
+            self.ser = serial.Serial(port, 115200, timeout=1)
+
+        self.last_time = None
+        self.last_sample = None
+
+    def process(self) -> Dict[str, Tuple[np.ndarray, Dict[str, Any]]]:
+        data_buf, time_buf = [], []
 
         packet = bytearray()
-        while True:
-            c = ser.read()
-            if c == END:
-                if packet:  # ignore empty packets
-                    if len(packet) == 2:
-                        value = packet[0] << 8 | packet[1]
-                        current_time = time.time()
-                        serial_buffer.append(value)
-                        buffer_times.append(current_time)
-                        break
-                    else:
-                        print("Invalid packet received")
-                    packet = bytearray()
-            elif c == ESC:
-                c = ser.read()
-                if c == ESC_END:
+        start = time.time()
+        while time.time() - start < 1 / self.params.common.max_frequency.value:
+            c = self.ser.read()
+            if c == self.END:
+                # ignore empty packets
+                if packet and len(packet) == 2:
+                    value = packet[0] << 8 | packet[1]
+                    current_time = time.time()
+                    data_buf.append(value)
+                    time_buf.append(current_time)
+                packet = bytearray()
+            elif c == self.ESC:
+                c = self.ser.read()
+                if c == self.ESC_END:
                     packet.append(0xC0)
-                elif c == ESC_ESC:
+                elif c == self.ESC_ESC:
                     packet.append(0xDB)
                 else:
                     print("Invalid escape sequence")
             elif len(c) > 0:
                 packet.append(c[0])
-        return serial_buffer, buffer_times
 
-    def process(self) -> Dict[str, Tuple[np.ndarray, Dict[str, Any]]]:
-        data, times = self.read_serial()
-        data = np.array(data)
-        times = np.array(times)
-        # Additional logic and processing.
+        if len(data_buf) == 0:
+            # no data received
+            return None
+
+        if self.last_time is None:
+            # first time, just store the data
+            self.last_time = time_buf[-1]
+            self.last_sample = data_buf[-1]
+            return None
+
+        # resample the data
+        dt = 1 / self.params.serial.sfreq.value
+        xs = np.arange(time_buf[0], time_buf[-1], dt)
+        # TODO: make sure the time array is correct and we don't have discontinuities
+        data = np.interp(xs, time_buf, data_buf, left=self.last_sample)
+
+        self.last_time = time_buf[-1]
+        self.last_sample = data[-1]
 
         meta = {"sfreq": self.params.serial.sfreq.value, "dim0": ["serial"]}
-        return {"out": (data, meta)}  # Here, data is your processed data, modify as needed.
+        return {"out": (data, meta)}
 
-    def detect_serial_port(self, name="Arduino"):
+    def detect_serial_port(self, names=["Arduino", "Serial"]):
+        # detect the serial port
         ports = serial.tools.list_ports.comports()
         for port in ports:
-            if name in port.description or "Serial" in port.description:
-                return port.device
+            for name in names:
+                if name in port.description:
+                    return port.device
         return None
+
+    def serial_port_changed(self, value: str) -> None:
+        # reinitialize the client
+        self.setup()
