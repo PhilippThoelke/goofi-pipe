d4459161a166a1b0fb17138826b13529e3145b80
AntoineBellemare <antoine.bellemare9@gmail.com>
2023-09-29 23:15:31 -0400

    add classical measures of connectivity

--- src/goofi/nodes/analysis/connectivity.py
+++ src/goofi/nodes/analysis/connectivity.py
@@ -1,8 +1,9 @@
 import numpy as np
-
+from scipy.signal import hilbert, coherence
+from scipy.stats import zscore
 from goofi.data import Data, DataType
 from goofi.node import Node
-from goofi.params import FloatParam, IntParam
+from goofi.params import FloatParam, IntParam, StringParam
 
 
 class Connectivity(Node):
@@ -17,14 +18,16 @@ class Connectivity(Node):
     def config_params():
         return {
             "biotuner": {
-                "method": "harmsim",
+                "method": StringParam("None", options=["None", "harmsim", "euler", "subharm_tension", "RRCi", "wPLI_crossfreq"]),
                 "n_peaks": IntParam(5, 1, 10),
                 "f_min": FloatParam(2.0, 0.1, 50.0),
                 "f_max": FloatParam(30.0, 1.0, 100.0),
                 "precision": FloatParam(0.1, 0.01, 10.0),
                 "peaks_function": "EMD",
-            }
-        }
+            },
+            "classical": {
+                "method": StringParam("wPLI", options=["coherence", "wPLI", "PLI", "imag_coherence", "PLV"]),
+        }}
 
     def process(self, data: Data):
         if data is None:
@@ -33,19 +36,25 @@ class Connectivity(Node):
         data.data = np.squeeze(data.data)
         if data.data.ndim != 2:
             raise ValueError("Data must be 2D")
+        
+        if self.params["biotuner"]["method"].value != "None":
+            matrix = compute_conn_matrix_single(
+                data.data,
+                data.meta["sfreq"],
+                peaks_function=self.params["biotuner"]["peaks_function"].value,
+                min_freq=self.params["biotuner"]["f_min"].value,
+                max_freq=self.params["biotuner"]["f_max"].value,
+                precision=self.params["biotuner"]["precision"].value,
+                n_peaks=self.params["biotuner"]["n_peaks"].value,
+                metric=self.params["biotuner"]["method"].value,
+            )
 
-        harm_conn = compute_conn_matrix_single(
-            data.data,
-            data.meta["sfreq"],
-            peaks_function=self.params["biotuner"]["peaks_function"].value,
-            min_freq=self.params["biotuner"]["f_min"].value,
-            max_freq=self.params["biotuner"]["f_max"].value,
-            precision=self.params["biotuner"]["precision"].value,
-            n_peaks=self.params["biotuner"]["n_peaks"].value,
-            metric=self.params["biotuner"]["method"].value,
-        )
+        if self.params["biotuner"]["method"].value == "None":
+            method = self.params["classical"]["method"].value
+            matrix = compute_classical_connectivity(data.data, method)
 
-        return {"matrix": (harm_conn, data.meta)}
+                
+        return {"matrix": (matrix, data.meta)}
 
 
 connectivity_fn = None
@@ -80,3 +89,39 @@ def compute_conn_matrix_single(
     )
     bt_conn.compute_harm_connectivity(metric=metric, save=False, graph=False)
     return bt_conn.conn_matrix
+
+def compute_classical_connectivity(data, method):
+    n_channels, n_samples = data.shape
+    matrix = np.zeros((n_channels, n_channels))
+    for i in range(n_channels):
+        for j in range(i, n_channels):  # Only compute upper diagonal
+            if i == j:
+                matrix[i, j] = 1  # diagonal elements are 1, for coherence/PLV or 0 for others, adjust as needed
+                continue
+
+            if method == "wPLI":
+                sig1 = hilbert(data[i, :])
+                sig2 = hilbert(data[j, :])
+                imag_csd = np.imag(np.exp(1j * (np.angle(sig1) - np.angle(sig2))))
+                matrix[i, j] = matrix[j, i] = np.abs(np.mean(imag_csd)) / np.mean(np.abs(imag_csd))
+            
+            elif method == "coherence":
+                f, Cxy = coherence(data[i, :], data[j, :])
+                matrix[i, j] = matrix[j, i] = np.mean(Cxy)
+            
+            elif method == "PLI":
+                sig1 = hilbert(data[i, :])
+                sig2 = hilbert(data[j, :])
+                matrix[i, j] = matrix[j, i] = np.mean(np.sign(np.angle(sig1) - np.angle(sig2)))
+            
+            elif method == "imag_coherence":
+                sig1 = hilbert(data[i, :])
+                sig2 = hilbert(data[j, :])
+                matrix[i, j] = matrix[j, i] = np.mean(np.imag(np.conj(sig1)*sig2)) / (np.sqrt(np.mean(np.imag(sig1)**2)) * np.sqrt(np.mean(np.imag(sig2)**2)))
+            
+            elif method == "PLV":
+                sig1 = hilbert(data[i, :])
+                sig2 = hilbert(data[j, :])
+                matrix[i, j] = matrix[j, i] = np.abs(np.mean(np.exp(1j * (np.angle(sig1) - np.angle(sig2)))))
+    
+    return matrix
