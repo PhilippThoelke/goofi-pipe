f52e43d9c2c4518a8851494828604e67854ef8c8
PhilippThoelke <philipp.thoelke@posteo.de>
2023-10-01 05:18:44 -0400

    Fix recurrence node

--- src/goofi/nodes/signal/recurrence.py
+++ src/goofi/nodes/signal/recurrence.py
@@ -1,11 +1,12 @@
 import numpy as np
 from sklearn.metrics import pairwise_distances
-from goofi.node import Node
+
 from goofi.data import Data, DataType
+from goofi.node import Node
 from goofi.params import FloatParam
 
-class Recurrence(Node):
 
+class Recurrence(Node):
     def config_input_slots():
         return {"input_array": DataType.ARRAY}
 
@@ -15,32 +16,28 @@ class Recurrence(Node):
     def config_params():
         return {
             "recurrence": {
-                "threshold": FloatParam(0.1, 0.0, 10.0), # You can adjust the bounds as per your needs
+                "threshold": FloatParam(0.1, 0.0, 10.0),  # You can adjust the bounds as per your needs
             }
         }
 
     def process(self, input_array: Data):
         if input_array is None or input_array.data is None:
             return None
-        
+
         array = input_array.data
-        
+
         if array.shape[0] < 2:
             print("Not enough data points to compute recurrence matrix.")
             return None
-        
+
         threshold = self.params["recurrence"]["threshold"].value
-        
+
         # Compute pairwise distance
         distance_matrix = pairwise_distances(array)
-        print(distance_matrix)
         # Handling potential NaN or Inf values
-        #if np.any(np.isnan(distance_matrix)) or np.any(np.isinf(distance_matrix)):
+        # if np.any(np.isnan(distance_matrix)) or np.any(np.isinf(distance_matrix)):
         #    print("Warning: The distance matrix contains NaN or Inf values. They will be replaced with a large number.")
         #    distance_matrix = np.nan_to_num(distance_matrix, nan=np.inf)
         #    distance_matrix = np.clip(distance_matrix, a_min=None, a_max=np.finfo(np.float32).max)
-        
-        # Thresholding to compute the recurrence matrix
-        recurrence_matrix = distance_matrix < threshold
-        
-        return {"recurrence_matrix": (recurrence_matrix, input_array.meta)}
+
+        return {"recurrence_matrix": (distance_matrix, input_array.meta)}
