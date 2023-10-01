58819861b531263006300bdde8447c7efadbaa96
PhilippThoelke <philipp.thoelke@posteo.de>
2023-10-01 18:13:26 -0400

    Add method dropdown to reduce

--- src/goofi/nodes/array/reduce.py
+++ src/goofi/nodes/array/reduce.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from goofi.data import Data, DataType
 from goofi.node import Node
+from goofi.params import StringParam
 
 
 class Reduce(Node):
@@ -12,16 +13,15 @@ class Reduce(Node):
         return {"out": DataType.ARRAY}
 
     def config_params():
-        return {"reduce": {"method": "mean", "axis": 0}}
+        return {"reduce": {"method": StringParam("mean", options=["mean", "median", "min", "max", "std"]), "axis": 0}}
 
     def process(self, array: Data):
         if array is None:
             return None
 
-        if not hasattr(np, self.params.reduce.method.value):
-            raise ValueError(f"Unknown reduce method {self.params.reduce.method.value}. Supported are all NumPy methods.")
-
-        result = getattr(np, self.params.reduce.method.value)(array.data, axis=self.params.reduce.axis.value)
-        if not isinstance(result, np.ndarray):
-            result = np.array(result)
-        return {"out": (result, array.meta)}
+        return {
+            "out": (
+                getattr(np, self.params.reduce.method.value)(array.data, axis=self.params.reduce.axis.value),
+                array.meta,
+            )
+        }
