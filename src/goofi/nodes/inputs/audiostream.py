2e4ec86e408a4c6fc7f567549d9c90032fd08097
PhilippThoelke <philipp.thoelke@posteo.de>
2023-10-01 04:12:35 -0400

    Squeeze audio buffer

--- src/goofi/nodes/inputs/audiostream.py
+++ src/goofi/nodes/inputs/audiostream.py
@@ -1,9 +1,11 @@
 import time
-import sounddevice as sd
+
 import numpy as np
+import sounddevice as sd
+
 from goofi.data import DataType
 from goofi.node import Node
-from goofi.params import IntParam, FloatParam, StringParam
+from goofi.params import FloatParam, IntParam, StringParam
 
 
 class AudioStream(Node):
@@ -13,38 +15,34 @@ class AudioStream(Node):
                 "sfreq": IntParam(44100, 8000, 192000),
                 "buffer_seconds": FloatParam(5.0, 1.0, 60.0),
                 "channels": IntParam(1, 1, 10),
-                "device": StringParam("", options=AudioStream.list_audio_devices()),
-                "convert_to_mono": True, #TO DO fix this
+                "device": StringParam(AudioStream.list_audio_devices()[0], options=AudioStream.list_audio_devices()),
+                "convert_to_mono": True,  # TO DO fix this
             },
-            "common": {
-                "autotrigger": True
-            }
+            "common": {"autotrigger": True},
         }
-        
+
     def config_output_slots():
         return {"out": DataType.ARRAY}
-    
+
     def audio_device_changed(self, value):
         self.setup()
+
     def setup(self):
-        if hasattr(self, 'stream') and self.stream:
+        if hasattr(self, "stream") and self.stream:
             self.stream.stop()
             self.stream.close()
         self.sfreq = self.params["audio"]["sfreq"].value
         self.buffer_seconds = self.params["audio"]["buffer_seconds"].value
         self.channels = self.params["audio"]["channels"].value
         device = self.params["audio"]["device"].value or None  # corrected the way to access parameters.
-        
+
         print(f"Initializing stream with device={device}, sfreq={self.sfreq}, channels={self.channels}")  # Debug print
-        
+
         self.buffer = np.zeros((self.channels, int(self.sfreq * self.buffer_seconds)))
-        
+
         try:
             self.stream = sd.InputStream(
-                callback=self.audio_callback,
-                samplerate=self.sfreq, 
-                channels=self.channels, 
-                device=device
+                callback=self.audio_callback, samplerate=self.sfreq, channels=self.channels, device=device
             )
             self.stream.start()
         except Exception as e:
@@ -56,31 +54,30 @@ class AudioStream(Node):
             print(status)
         self.buffer = np.roll(self.buffer, shift=-frames, axis=1)
         self.buffer[:, -frames:] = indata.T  # Assuming you want to transpose the samples
-        
+
     def process(self):
         if not self.stream:
             print("Audio stream is not available.")
             return None
-          
+
         data = np.copy(self.buffer)
         # Convert to mono if required
         convert_to_mono = self.params.audio.convert_to_mono.value
         if convert_to_mono and self.channels > 1:
             data = np.mean(data, axis=0, keepdims=False)
             # ensure that the data is 1D
-            data = np.squeeze(data)     
+        data = np.squeeze(data)
         meta = {"sfreq": self.sfreq, "dim0": ["audio"]}
         return {"out": (data, meta)}
-    
+
     @staticmethod
     def list_audio_devices():
         devices = sd.query_devices()
         device_names = []
         for device in devices:
-            if device['max_input_channels'] > 0:  # This condition will check if a device is an input device.
-                device_names.append(device['name'])
+            if device["max_input_channels"] > 0:  # This condition will check if a device is an input device.
+                device_names.append(device["name"])
         print("Available Audio Input Devices:")
         for name in device_names:
             print(name)
         return device_names
-
