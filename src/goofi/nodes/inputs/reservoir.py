9ba4e2c01c3156d42251fcbbfa9a8eb4d78eba43
Philipp Thölke <philipp.thoelke@posteo.de>
2023-10-06 23:43:18 -0400

    Add connectivity input

--- src/goofi/nodes/inputs/reservoir.py
+++ src/goofi/nodes/inputs/reservoir.py
@@ -9,6 +9,9 @@ class Reservoir(Node):
     def config_params():
         return {"reservoir":{"size": IntParam(100,2,1000), "function": StringParam("tanh", options=["tanh", "sigmoid"]),"add_bias": True,"mean":FloatParam(0.0, -.2,.2),"std":FloatParam(1.0, 0.7, 1.3),},"common":{"autotrigger":True}}
 
+    def config_input_slots():
+        return {"connectivity": DataType.ARRAY}
+
     def config_output_slots():
         return {"data": DataType.ARRAY}
 
@@ -17,8 +20,21 @@ class Reservoir(Node):
         self.weights = np.random.uniform(-1, 1, (self.params.reservoir.size.value, self.params.reservoir.size.value))
         self.bias = np.random.uniform(-1, 1, self.params.reservoir.size.value)
 
-    def process(self):
-        w = self.weights*self.params.reservoir.std.value+self.params.reservoir.mean.value
+    def process(self, connectivity: Data):
+
+        if connectivity is None:
+            w = self.weights
+        else:
+            assert connectivity.data.ndim == 2, "Connectivity matrix must be two-dimensional."
+            assert connectivity.data.shape[0] == connectivity.data.shape[1], "Connectivity matrix must be square."
+            assert connectivity.data.shape[0] == self.params.reservoir.size.value, "Connectivity matrix must have the same size as the reservoir."
+            conn = connectivity.data
+            conn[np.diag_indices_from(conn)] = 0
+            w = conn
+
+        # scale the weights
+        w = w * self.params.reservoir.std.value + self.params.reservoir.mean.value
+
         self.reservoir = np.dot(w, self.reservoir)
         if self.params.reservoir.add_bias.value:
             self.reservoir += self.bias
