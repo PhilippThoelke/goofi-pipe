109c51ee4348ac5bc23b64e7a312fc81a163b797
AntoineBellemare <antoine.bellemare9@gmail.com>
2023-10-11 12:18:35 -0400

    added waveforms output

--- src/goofi/nodes/inputs/kuramoto.py
+++ src/goofi/nodes/inputs/kuramoto.py
@@ -8,14 +8,15 @@ class Kuramoto(Node):
         return {"initial_phases": DataType.ARRAY}
 
     def config_output_slots():
-        return {"phases": DataType.ARRAY, "coupling": DataType.ARRAY, "order_parameter": DataType.ARRAY}
+        return {"phases": DataType.ARRAY, "coupling": DataType.ARRAY, "order_parameter": DataType.ARRAY,
+                "waveforms": DataType.ARRAY}
 
     def config_params():
         return {
             "kuramoto": {
                 "coupling_strength": FloatParam(0.5, 0.0, 10.0),
                 "natural_frequencies": StringParam('1, 1.5, 2', doc="Comma-separated list of natural frequencies"),
-                "timesteps": IntParam(1000, 0, 10000, doc="Number of timesteps to integrate")
+                "timesteps": IntParam(1000, 0, 10000, doc="Number of timesteps to integrate"),
             }
         }
 
@@ -36,15 +37,20 @@ class Kuramoto(Node):
         else:
             theta = initial_phases.data
 
+        # Store phases at each timestep
+        theta_history = np.zeros((N, timesteps))
+        time_points = np.linspace(0, dt * timesteps, timesteps)
+
         # Integrate over the specified number of timesteps
-        for _ in range(timesteps):
+        for i in range(timesteps):
             coupling_term = np.sum(np.sin(theta - theta[:, np.newaxis]), axis=1) / N
             dtheta = dt * (omega + K * coupling_term)
             theta += dtheta
             theta = np.mod(theta, 2*np.pi)
+            theta_history[:, i] = theta
 
-        # Calculate the order parameter
-        R = np.abs(np.mean(np.exp(1j * theta)))
+        # Compute actual waveforms from the phase history
+        waveforms = np.sin(np.outer(omega, time_points) + theta_history)
 
         # Calculate the order parameter
         R = np.abs(np.mean(np.exp(1j * theta)))
@@ -54,5 +60,5 @@ class Kuramoto(Node):
 
         return {"phases": (theta.reshape(-1, 1), meta), 
                 "coupling": (coupling_term.reshape(-1, 1), meta), 
-                "order_parameter": (np.array(R), {})}
-
+                "order_parameter": (np.array(R), {}),
+                "waveforms": (waveforms, meta)}
