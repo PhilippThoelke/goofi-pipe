3e9d1717d221648816dbae3bf12c643742d72465
PhilippThoelke <philipp.thoelke@posteo.de>
2023-10-11 20:49:47 -0400

    Join type is a dropdown

--- src/goofi/nodes/array/join.py
+++ src/goofi/nodes/array/join.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from goofi.data import Data, DataType
 from goofi.node import Node
+from goofi.params import StringParam
 
 
 class Join(Node):
@@ -12,7 +13,7 @@ class Join(Node):
         return {"out": DataType.ARRAY}
 
     def config_params():
-        return {"join": {"method": "concatenate", "axis": 0}}
+        return {"join": {"method": StringParam("concatenate", options=["concatenate", "stack"]), "axis": 0}}
 
     def process(self, a: Data, b: Data):
         if a is None or b is None:
