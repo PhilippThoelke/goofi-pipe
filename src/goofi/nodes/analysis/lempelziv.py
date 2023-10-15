ad1f688d52e776c076ec2cf8a359cb52b924e127
Philipp Thölke <philipp.thoelke@posteo.de>
2023-10-15 15:14:27 -0400

    Improve readability

--- src/goofi/nodes/analysis/lempelziv.py
+++ src/goofi/nodes/analysis/lempelziv.py
@@ -13,12 +13,17 @@ class LempelZiv(Node):
         return {"lzc": DataType.ARRAY}
 
     def config_params():
-        return {"lempel_ziv": {"binarization":StringParam("mean",options=["mean","median"]),"axis": -1}}
+        return {
+            "lempel_ziv": {
+                "binarization": StringParam("mean", options=["mean", "median"]),
+                "axis": -1,
+            }
+        }
 
     def setup(self):
         from antropy import lziv_complexity
 
-        self.lzc_fn = lziv_complexity
+        self.compute_lzc = lziv_complexity
 
     def process(self, data: Data):
         if data is None:
@@ -35,6 +40,6 @@ class LempelZiv(Node):
             raise ValueError("Unknown binarization method")
 
         # compute normalized Lempel-Ziv complexity
-        lzc = np.apply_along_axis(self.lzc_fn, self.params.lempel_ziv.axis.value, binarized, normalize=True)
+        lzc = np.apply_along_axis(self.compute_lzc, self.params.lempel_ziv.axis.value, binarized, normalize=True)
 
         return {"lzc": (lzc, data.meta)}
