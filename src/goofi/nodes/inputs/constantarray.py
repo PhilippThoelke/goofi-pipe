36880b33b9016acfc17ae85c883954166f85bb3f
AntoineBellemare <antoine.bellemare9@gmail.com>
2023-10-18 15:23:30 -0400

    added ring graph and random array options

--- src/goofi/nodes/inputs/constantarray.py
+++ src/goofi/nodes/inputs/constantarray.py
@@ -2,20 +2,38 @@ import numpy as np
 
 from goofi.data import DataType
 from goofi.node import Node
-from goofi.params import FloatParam
-
+from goofi.params import FloatParam, StringParam
 
 class ConstantArray(Node):
     def config_params():
         return {
-            "constant": {"value": FloatParam(1.0, -10.0, 10.0), "shape": "1"},
+            "constant": {"value": FloatParam(1.0, -10.0, 10.0), "shape": "1", 
+                         "graph": StringParam('none', options=['none', 'ring', 'random'])},   
             "common": {"autotrigger": True},
+            
         }
 
     def config_output_slots():
         return {"out": DataType.ARRAY}
 
     def process(self):
-        parts = [p for p in self.params.constant.shape.value.split(",") if len(p) > 0]
-        shape = list(map(int, parts))
-        return {"out": (np.ones(shape) * self.params.constant.value.value, {"sfreq": self.params.common.max_frequency.value})}
+        if self.params.constant.graph.value == 'ring':
+            matrix = ring_graph_adjacency_matrix(int(self.params.constant.shape.value))
+            return {"out": (matrix, {"sfreq": self.params.common.max_frequency.value})}
+        elif self.params.constant.graph.value == 'random':
+            return {"out": (np.random.rand(int(self.params.constant.shape.value), int(self.params.constant.shape.value)), {"sfreq": self.params.common.max_frequency.value})}
+        else:
+            parts = [p for p in self.params.constant.shape.value.split(",") if len(p) > 0]
+            shape = list(map(int, parts))
+            return {"out": (np.ones(shape) * self.params.constant.value.value, {"sfreq": self.params.common.max_frequency.value})}
+
+def ring_graph_adjacency_matrix(n):
+    # Create an nxn zero matrix
+    adjacency = np.zeros((n, n), dtype=int)
+
+    # Set values for the ring connections
+    for i in range(n):
+        adjacency[i][(i + 1) % n] = 1  # Next vertex in the ring
+        adjacency[i][(i - 1) % n] = 1  # Previous vertex in the ring
+
+    return adjacency
