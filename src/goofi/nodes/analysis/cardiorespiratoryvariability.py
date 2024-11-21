bd542037ff7ba670a696f3e2f634520576d562d6
Philipp Thölke <philipp.thoelke@posteo.de>
2024-11-20 23:42:00 -0600

    Add extracted peaks as output

--- src/goofi/nodes/analysis/cardiorespiratoryvariability.py
+++ src/goofi/nodes/analysis/cardiorespiratoryvariability.py
@@ -21,6 +21,7 @@ class CardioRespiratoryVariability(Node):
             "HF": DataType.ARRAY,
             "LF/HF": DataType.ARRAY,
             "LZC": DataType.ARRAY,
+            "Peaks": DataType.ARRAY,
         }
 
     def config_params():
@@ -42,20 +43,24 @@ class CardioRespiratoryVariability(Node):
         if data.data.ndim > 1:
             raise ValueError("Data must be 1D")
 
+        cleaned = None
         if self.params["cardiorespiratory"]["input_type"].value == "ppg":
             datatype = "HRV"
-            _, info = self.neurokit.ppg_process(data.data, sampling_rate=data.meta["sfreq"])
+            ppg, info = self.neurokit.ppg_process(data.data, sampling_rate=data.meta["sfreq"])
             variability_df = self.neurokit.hrv(info, sampling_rate=data.meta["sfreq"])
+            cleaned = ppg["PPG_Peaks"].values
         elif self.params["cardiorespiratory"]["input_type"].value == "ecg":
             datatype = "HRV"
             # extract peaks
-            _, info = self.neurokit.ecg_peaks(data.data, sampling_rate=data.meta["sfreq"])
+            ecg, info = self.neurokit.ecg_peaks(data.data, sampling_rate=data.meta["sfreq"])
             # compute hrv
             variability_df = self.neurokit.hrv(info, sampling_rate=data.meta["sfreq"])
+            cleaned = ecg["ECG_Peaks"].values
         elif self.params["cardiorespiratory"]["input_type"].value == "rsp":
             datatype = "RRV"
-            _, info = self.neurokit.rsp_process(data.data, sampling_rate=data.meta["sfreq"])
+            rsp, info = self.neurokit.rsp_process(data.data, sampling_rate=data.meta["sfreq"])
             variability_df = self.neurokit.rsp_rrv(data.data, sampling_rate=data.meta["sfreq"])
+            cleaned = rsp["RSP_Peaks"].values
 
         BBorNN = "BB" if datatype == "RRV" else "NN"
         pNNorBBx = "pBBx" if datatype == "RRV" else "pNN50"
@@ -71,4 +76,5 @@ class CardioRespiratoryVariability(Node):
             "HF": (np.array(variability_df[f"{datatype}_HF"]), {}),
             "LF/HF": (np.array(variability_df[f"{datatype}_LFHF"]), {}),
             "LZC": (np.array(variability_df[f"{datatype}_LZC"]), {} if datatype == "HRV" else None),
+            "Peaks": (cleaned, data.meta),
         }
