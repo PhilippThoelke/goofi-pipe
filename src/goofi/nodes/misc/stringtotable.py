030f312a219590f94f2bc2c1472332d7bd9556bd
Philipp Thölke <philipp.thoelke@posteo.de>
2024-12-15 19:52:04 -0500

    Add string to table node

---
+++ src/goofi/nodes/misc/stringtotable.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from goofi.data import Data, DataType
+from goofi.node import Node
+from goofi.params import BoolParam, StringParam
+
+
+class StringToTable(Node):
+    def config_input_slots():
+        return {"text": DataType.STRING}
+
+    def config_output_slots():
+        return {"table": DataType.TABLE}
+
+    def config_params():
+        return {
+            "string_to_table": {
+                "format": StringParam("json", options=["json", "yaml"], doc="Input text format"),
+                "clean_backslashes": BoolParam(True, doc="Remove backslashes before quotes"),
+            }
+        }
+
+    def process(self, text):
+        if text.data is None:
+            return None
+
+        meta = text.meta
+        text = text.data
+        if self.params.string_to_table.clean_backslashes.value:
+            text = text.replace('\\"', '"')
+
+        if self.params.string_to_table.format.value == "json":
+            try:
+                import json
+
+                table = json.loads(text)
+            except json.JSONDecodeError as e:
+                raise ValueError(f"Error decoding JSON: {e}")
+        elif self.params.string_to_table.format.value == "yaml":
+            try:
+                import yaml
+
+                table = yaml.safe_load(text)
+            except yaml.YAMLError as e:
+                raise ValueError(f"Error decoding YAML: {e}")
+        else:
+            raise ValueError(f"Unsupported format: {self.params['string_to_table']['format'].value}")
+
+        # parse the table and return it
+        return {"table": (parse_table(table, meta), meta)}
+
+
+def parse_table(table, meta):
+    for key, value in table.items():
+        if isinstance(value, dict):
+            table[key] = Data(DataType.TABLE, parse_table(value), meta)
+
+        try:
+            table[key] = Data(DataType.ARRAY, np.array(float(value)), meta)
+            continue
+        except Exception:
+            pass
+        
+        table[key] = Data(DataType.STRING, str(value), meta)
+
+    return table
