3d5963e6aec97e73d8cfe397049b6c90de6359ae
Philipp Thölke <philipp.thoelke@posteo.de>
2024-12-28 16:38:34 -0500

    Add embedding csv option

--- src/goofi/nodes/inputs/loadfile.py
+++ src/goofi/nodes/inputs/loadfile.py
@@ -7,18 +7,19 @@ from goofi.params import FloatParam, StringParam
 
 class LoadFile(Node):
     def config_output_slots():
-        return {"data_output": DataType.ARRAY}
+        return {"data_output": DataType.ARRAY, "string_output": DataType.STRING}
 
     def config_params():
         return {
             "file": {
                 "filename": StringParam("earth", doc="The name of the file to load with extension"),
                 "type": StringParam(
-                    "spectrum",
-                    options=["spectrum", "time_series", "ndarray"],
-                    doc="Type of file to load, choose between 'ndarray', 'time_series' or 'spectrum'",
+                    "spectrum", options=["spectrum", "time_series", "ndarray", "embedding_csv"], doc="Type of file to load"
                 ),
                 "freq_multiplier": FloatParam(1.0, doc="Multiplier to adjust the frequency values"),
+                "header": 0,
+                "index_column": True,
+                "select": StringParam("", doc="NumPy selection string"),
             },
             "common": {"autotrigger": True},
         }
@@ -35,18 +36,52 @@ class LoadFile(Node):
     def process(self):
         if self.params.file.filename.value is None:
             return None
+
         # asset_path = self.assets_path
         file_type = self.params["file"]["type"].value
         filename = self.params["file"]["filename"].value
         extension = filename.split(".")[-1]
+
+        df = None
         if extension == "npy":
             data = np.load(f"{filename}", allow_pickle=True)
         elif extension == "txt":
             data = np.loadtxt(f"{filename}")
         elif extension == "csv":
-            data = self.pd.read_csv(f"{filename}", header=None).values
-        if data.shape[0] > data.shape[1]:
-            data = data.T
+            df = self.pd.read_csv(
+                f"{filename}",
+                header=self.params.file.header.value,
+                index_col=0 if self.params.file.index_column.value else None,
+            )
+            data = df.values
+
+        # apply selection
+        selection = self.params.file.select.value
+        dtypes = None
+        if selection:
+            try:
+                # Parse the selection string into a tuple of slices
+                slices = tuple(
+                    (
+                        slice(*map(lambda x: int(x.strip()) if x.strip() else None, dim.split(":")))
+                        if ":" in dim
+                        else int(dim.strip())
+                    )
+                    for dim in selection.split(",")
+                )
+                data = data[slices]
+                if df is not None:
+                    dtypes = list(df.dtypes)[slices[1]]
+                    if not isinstance(dtypes, list):
+                        dtypes = [dtypes]
+            except (ValueError, IndexError) as e:
+                print(f"Invalid selection string: {selection}")
+
+        print([str(d) for d in dtypes], dtypes is not None and any([dtype == "object" for dtype in dtypes]))
+        if dtypes is not None and any([dtype == "object" for dtype in dtypes]):
+            return {"data_output": None, "string_output": ("\n".join(data), {})}
+
+        data = data.astype(np.float32)
 
         # Handle time_series type
         if file_type == "time_series":
@@ -55,7 +90,7 @@ class LoadFile(Node):
                 self.time_series, self.meta = data[0], data[1]
                 assert isinstance(self.meta, dict), "Metadata should be a dictionary"
 
-            return {"data_output": (self.time_series, self.meta)}
+            return {"data_output": (self.time_series, self.meta), "string_output": None}
 
         # Handle spectrum type
         elif file_type == "spectrum":
@@ -63,9 +98,12 @@ class LoadFile(Node):
             freq_vector = data[0] * freq_multiplier  # Multiply the frequency values
             spectrums = data[1]
 
-            return {"data_output": (np.array(spectrums), {"freq": freq_vector})}
+            return {"data_output": (np.array(spectrums), {"freq": freq_vector}), "string_output": None}
 
         elif file_type == "ndarray":
-            return {"data_output": (data, {})}
+            return {"data_output": (data, {}), "string_output": None}
+
+        elif file_type == "embedding_csv":
+            return {"data_output": (data, {}), "string_output": None}
 
         return None
